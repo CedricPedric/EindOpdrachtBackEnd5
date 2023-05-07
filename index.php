ccff6c8512d95c74fdfd5cf125028ce5289d61e1
CedricPedric <cedricnelson03@gmail.com>
2023-05-07 19:38:59 +0200

    de menu pagina heeft nu dymanische characters

--- index.php
+++ index.php
@@ -20,59 +20,27 @@
     <header><h1>Alle <?=count($characters)?> characters uit de database</h1></header>
     
     <div id="container">
-        <h1>Welcome!</h1>
-        <?php foreach($characters as $character){
-            echo $character['name'] . ", " ;
-        }
-        ?>
-        <!-- <a class="item" href="character.html">
+        <?php foreach($characters as $character){?>
+        <a class="item" href="character.php">
             <div class="left">
-                <img class="avatar" src="resources/images/bowser.jpg">
+                <img class="avatar" src=resources/images/<?=$character['avatar']?> >
             </div>
             <div class="right">
-                <h2>Bowser</h2>
+                <h2><?=$character['name']?></h2>
                 <div class="stats">
                     <ul class="fa-ul">
-                        <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
-                        <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
-                        <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
+                        <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?=$character['health']?></li>
+                        <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?=$character['attack']?></li>
+                        <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?=$character['defense']?></li>
                     </ul>
                 </div>
             </div>
             <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
-        </a> -->
+        </a>
+        <?}?>
     </div>
 
 
     <footer>&copy; [jenaam] 2023</footer>
 </body>
 </html>
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<body>
-<header><h1>Alle [X] characters uit de database</h1></header>
-<div id="container">
-    <a class="item" href="character.html">
-        <div class="left">
-            <img class="avatar" src="resources/images/bowser.jpg">
-        </div>
-        <div class="right">
-            <h2>Bowser</h2>
-            <div class="stats">
-                <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
-                    <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
-                    <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
-                </ul>
-            </div>
-        </div>
-        <div class="detailButton"><i class="fas fa-search"></i> bekijk</div>
-    </a>
-</div>
-
-
-<footer>&copy; [jenaam] 2023</footer>
-</body>
-</html> -->
